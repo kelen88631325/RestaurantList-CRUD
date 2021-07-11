ed28527d507e9495003dbd057bdaf2b3adeb8536
kelen8861325 <kelen8861325@outlook.com>
2021-07-11 22:40:43 +0800

    餐廳清單原版

---
+++ README.md
@@ -0,0 +1,42 @@
+# 餐廳清單
+
+餐廳清單主頁可看到所以餐廳列表，
+提供查閱餐廳資訊與查詢餐廳功能。
+
+## 產品功能
+
+1、可查看所有餐廳列表，
+
+2、點擊顯示餐廳詳細資訊，包含名稱、電話、地址與圖片等。
+
+3、可查詢餐廳名稱，或者餐廳種類。
+
+## 環境建置
+
+- Visual Studio Code
+- Node.js
+- Express
+- Express-Handlebars
+- nodemon
+
+## 安裝步驟
+
+1、打開終端機(terminal)，Clone 此專案至本機電腦
+
+2、進入本專案資料夾，指令"cd <資料夾名稱>"
+
+3、安裝 npm，輸入 $ npm install
+
+4、安裝 nodemon 套件，輸入 $ npm install -g nodemon
+
+5、安裝模板引擎，輸入 $ npm i express-handlebars
+
+6、執行檔案 $ npm run dev
+
+7、終端機出現以下字樣"Express is listening on localhost: 3000."，代表成功
+
+8、開啟瀏覽器，輸入 http://localhost:3000，開始使用
+
+## 成品示意圖
+
+![示意圖](./示意圖.jpg)
--- app.js
+++ app.js
@@ -1,10 +1,52 @@
+// app.js
+// require packages used in the project
 const express = require("express");
 const app = express();
+const port = 3000;
+const exphbs = require("express-handlebars");
+const restaurantList = require("./restaurant.json");
 
+// setting template engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// 設定靜態檔案
+// setting static files
+app.use(express.static("public"));
+
+// 顯示主頁面
+//routes setting
 app.get("/", (req, res) => {
-  res.send("哈搂握的");
+  res.render("index", { restaurant: restaurantList.results });
 });
 
-app.listen(3000, () => {
-  console.log("App is running on port 3000.");
+// 顯示子頁面，設定子頁面網址
+app.get("/restaurants/:restaurant_id", (req, res) => {
+  console.log("restaurant_id", req.params.restaurant_id);
+  const restaurant = restaurantList.results.filter(function (restaurant) {
+    return restaurant.id == req.params.restaurant_id;
+  });
+  console.log("restaurant", restaurant);
+  res.render("show", { restaurant: restaurant[0] });
 });
+
+//搜尋功能，搜尋名稱或者餐廳類型
+app.get("/search", (req, res) => {
+  console.log("req keyword", req.query.keyword);
+  const keyword = req.query.keyword;
+  const restaurant = restaurantList.results.filter((restaurant) => {
+    return (
+      restaurant.name.toLowerCase().includes(keyword.trim().toLowerCase()) ||
+      restaurant.category.toLowerCase().includes(keyword.trim().toLowerCase())
+    );
+  });
+
+  res.render("index", { restaurant: restaurant, keyword: keyword });
+});
+
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on localhost:${port}`);
+});
+
+//2021.07.11分支測試
